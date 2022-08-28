a9d6b2c2c87ffd782b24e3c957e569cbe1930f25
JehanKandy <89431184+JehanKandy@users.noreply.github.com>
2022-08-28 18:28:49 +0530

    Update cal1.py

--- cal1.py
+++ cal1.py
@@ -25,4 +25,6 @@ while True:
             print(num1, "+", num2, "=", add(num1,num2))
         elif choice == '2':
             print(num1, "-", num2, "=", subtract(num1,num2))
+        elif choice == '3':
+            print(num1, "*", num2, "=", multiply(num1,num2))
     
